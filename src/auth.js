da94bd6d25285042546663928c4dc5df061cb159
Bilal <bilal.khurshid@ahead.com>
2025-04-20 05:55:41 -0500

    let's try that again

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,4 @@
+import './auth.js';
 const clientId = "c67e72384a114191b582401cda129f93";
 const redirectUri = "https://main.ddmdfp20v4puw.amplifyapp.com/callback.html/"; // or your actual domain
 const scopes = [
